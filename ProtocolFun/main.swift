dacfbc0256a49521f0708cbb7a8c62a48e1520dc
gsprint <sprint@gonzaga.edu>
2020-09-15 11:13:48 -0700

    Initial Commit

---
+++ ProtocolFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProtocolFun
+//
+//  Created by Gina Sprint on 9/15/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
